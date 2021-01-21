64142c7ca2a3a621655525c88bd341eb3ca46840
Kaushal Dhakal <kaushaldhakal40@gmail.com>
2021-01-21 15:17:47 +0545

    Setup Finished

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      Personal Portfolio
+      <h1>Kaushal Dhakal</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+        rerum fuga. Asperiores accusamus culpa adipisci, aperiam ut ipsum eum,
+        dolore aliquid deserunt qui quis. Quas eveniet commodi voluptatum
+        aliquam labore unde. Officiis, recusandae mollitia? Quidem voluptate
+        recusandae nobis voluptatem sapiente ratione quam odio autem
+        consequatur. Reiciendis molestias at voluptates accusamus!
+      </p>
+    </div>
+  );
+}
+
+export default App;
