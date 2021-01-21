4f759f69ad17b50a90b98f8a2af66443d212c0bb
Kaushal Dhakal <kaushaldhakal40@gmail.com>
2021-01-21 20:33:59 +0545

    Navbar going

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../Logo.PNG";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <div className="container-fluid">
+      <div className="container">
         <a className="navbar-brand" href="#">
           <img className="logo" src={logo} alt="no logo" />
         </a>
@@ -20,15 +20,41 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link1 active" aria-current="page" href="#">
+          <ul className="navbar-nav ml-auto ">
+            <li className="nav-item active">
+              <a className="nav-link " href="#">
                 Home
+                <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
                 Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                contacts
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                experience
               </a>
             </li>
           </ul>
