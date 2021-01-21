1d0d185623548e7684e39acaf3dd411050fe0ad0
Kaushal Dhakal <kaushaldhakal40@gmail.com>
2021-01-21 15:31:48 +0545

    main

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
-    <div>
-      Personal Portfolio
-      <h1>Kaushal Dhakal</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
-        rerum fuga. Asperiores accusamus culpa adipisci, aperiam ut ipsum eum,
-        dolore aliquid deserunt qui quis. Quas eveniet commodi voluptatum
-        aliquam labore unde. Officiis, recusandae mollitia? Quidem voluptate
-        recusandae nobis voluptatem sapiente ratione quam odio autem
-        consequatur. Reiciendis molestias at voluptates accusamus!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quam
-        obcaecati reprehenderit dignissimos, vel ex itaque necessitatibus odit
-        voluptates neque eius deleniti exercitationem doloremque inventore velit
-        pariatur veniam temporibus beatae magni ad placeat quo voluptatibus?
-        Exercitationem quo, eaque dolorum repellendus assumenda ratione
-        consequatur deleniti accusamus illum enim rerum commodi rem veniam
-        aspernatur laborum ipsam quas eum soluta! Fugiat beatae tempora
-        laboriosam excepturi soluta recusandae cumque error delectus assumenda
-        laborum, ut voluptatibus dicta quas dolore ad minima esse autem neque.
-        Soluta a beatae quam doloremque quod molestiae, repudiandae animi
-        deserunt corporis voluptates itaque quis in optio placeat suscipit odio
-        aliquam nostrum!
-      </p>
-    </div>
-  );
+  return <div>Portfolio coming soon....</div>;
 }
 
 export default App;
