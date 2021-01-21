de43e82266b0d4d2142f94797eea9df2df4235f1
Kaushal Dhakal <kaushaldhakal40@gmail.com>
2021-01-21 21:21:15 +0545

    working on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto ">
+          <ul className="navbar-nav  ">
             <li className="nav-item active">
               <a className="nav-link " href="#">
                 Home
